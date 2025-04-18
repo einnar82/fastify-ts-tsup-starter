86affc03779eed3e1fef6f1cc3641ad0bd1ea38e
Rannie Ollit <rannieollit@Rannies-MacBook-Pro.local>
2025-04-19 03:17:47 +0800

    added knex plugin

--- apps/backend/src/plugins/knex.plugin.ts
+++ apps/backend/src/plugins/knex.plugin.ts
@@ -1,24 +1,17 @@
-import fp from "fastify-plugin"
-import knex from "knex"
-import type { FastifyInstance } from "fastify"
+import fp from 'fastify-plugin';
+import knex from 'knex';
+import type { FastifyInstance, FastifyPluginOptions } from 'fastify';
 
-function knexPlugin(server: FastifyInstance, _options, next) {
+export default fp(async (server: FastifyInstance) => {
   const knexConnection = knex({
-    client: "pg",
+    client: 'pg',
     connection: process.env.DATABASE_URL,
-  })
+  });
 
-  server.decorate("knex", knexConnection)
+  server.decorate('knex', knexConnection);
 
-  server.addHook("onClose", (instance, done) => {
-    if (instance.knex === knexConnection) {
-      instance.knex.destroy()
-      delete instance.knex
-    }
-    done()
-  })
+  server.addHook('onClose', async (instance) => {
+    await knexConnection.destroy();
+  });
+});
 
-  next()
-}
-
-export default fp(knexPlugin)
