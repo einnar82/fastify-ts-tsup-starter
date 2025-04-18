fdd8432f04fcc3d5c9ba689b5bedf8f0020df1f2
Rannie Ollit <rannieollit@Rannies-MacBook-Pro.local>
2025-04-19 01:49:10 +0800

    setup monorepo

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -5,7 +5,7 @@ const app = await createServer();
 const port = Number(process.env.PORT) || 3000;
 
 try {
-  await app.listen({ port });
+  await app.listen({ port, host: "0.0.0.0" });
   console.log(`🚀 Server running at http://localhost:${port}`);
 } catch (err) {
   app.log.error(err);
